9878a534477e5025aeabcc8751a57fb89a41bb20
tie052312 <tie052312@S1611C11952-T1.tisnt.tis.co.jp>
2018-06-21 21:54:07 +0900

    コミット

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,9 +2,57 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
+
+		System.out.println(args[0]);
+
+		String in = null;
+		String output = null;
+
 		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+
+			if (i == 0) {
+				in = args[i];
+			} else {
+				in = in + args[i];
+			}
+
 		}
+
+		int check = Integer.parseInt(in);
+
+
+		// 1000以下の場合
+		if(check <= 1000) {
+
+			for (int i = 0; i < in.length(); i++) {
+
+				// 3のつく場合
+				if ("3" == String.valueOf(in.charAt(i))) {
+					// 3の倍数でかつ3のつく1000以下の非負整数の場合
+					if (0 == check % 3) {
+						output = "dumb";
+						// 3のつく1000以下の非負整数の場合
+					} else {
+						output = "stupid";
+					}
+
+					// 3のつかない場合
+				} else {
+					// 3の倍数の1000以下の非負整数の場合
+					if (0 == check % 3) {
+						output = "idiot";
+					} else {
+						output = "smart";
+					}
+
+				}
+			}
+
+		} else {
+			output = "invalid";
+		}
+
+			System.out.println(output);
+
 	}
 }

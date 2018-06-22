c89b6c9bb6684bb604894b43531b0168e238c716
tie052312 <tie052312@S1611C11952-T1.tisnt.tis.co.jp>
2018-06-22 20:56:40 +0900

    commit

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,101 +1,139 @@
 package codecheck;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 	public static void main(String[] args) {
 
-//		System.out.println(args[0]);
-
 		String separator = "\r\n";
 
-		String in = null;
-		String output = null;
+		String output = "";
 
 
-//		for (int i = 0, l = args[0].length(); i < l; i++) {
-//
-//			args[0].charAt(i);
-//
-//
-//			if (i == 0) {
-//				in = args[i];
-//			} else {
-//				in = in + args[i];
-//			}
-//
-//		}
+		List<String> paramList = new ArrayList<String>();
 
+		for (int i = 0; i < args.length; i++) {
+			paramList.add(args[i]);
+		}
 
-		String[] input ;
+		String check;
+		int num;
 
-		for (int i = 0; i < args[0].length(); i++) {
+		for ( int i =0; i < paramList.size(); i++) {
 
-			if (separator.equals(args[0].charAt(i))) {
+			try {
 
+				num =  Integer.parseInt(paramList.get(i));
+				check = paramList.get(i);
+				Boolean checkFlg = false;
 
+				// 1000以下の場合
+				if(num <= 1000) {
 
-			} else {
+					for (int j = 0; j < check.length(); j++) {
 
+						// 3のつく場合
+						if ("3".equals(String.valueOf(check.charAt(j)))) {
+							// 3の倍数でかつ3のつく1000以下の非負整数の場合
+							if (num % 3 == 0) {
 
+								if (output.isEmpty()) {
+									output = "dumb";
+								} else {
+
+									//TODO
+									System.out.println(output);
+
+									output = output + separator + "dumb";
+
+									//TODO
+									System.out.println(output);
+
+								}
+								checkFlg = true;
+								break;
+								// 3のつく1000以下の非負整数の場合
+							} else {
+
+								if (output.isEmpty()) {
+									output = "stupid";
+								} else {
+									//TODO
+									System.out.println(output);
+
+									output = output + separator + "stupid";
+
+									//TODO
+									System.out.println(output);
+								}
+								checkFlg = true;
+								break;
+
+							}
+						}
+					}
+
+					// 3のつかない場合
+					if(!checkFlg) {
+						// 3の倍数の1000以下の非負整数の場合
+						if (num % 3 == 0) {
+
+							if (output.isEmpty()) {
+								output = "idiot";
+							} else {
+
+								//TODO
+								System.out.println(output);
+								output = output + separator + "idiot";
+
+								//TODO
+								System.out.println(output);
+
+							}
+							checkFlg = true;
+
+						} else {
+
+							if (output.isEmpty()) {
+								output = "smart";
+							} else {
+
+								//TODO
+								System.out.println(output);
+								output = output + separator + "smart";
+
+								//TODO
+								System.out.println(output);
+
+							}
+							checkFlg = true;
+
+						}
+					}
+				} else {
+
+					if (output.isEmpty()) {
+						output = "invalid";
+					} else {
+
+						//TODO
+						System.out.println(output);
+						output = output + separator + "invalid";
+						//TODO
+						System.out.println(output);
+
+					}
+					checkFlg = true;
+				}
+
+			} catch (NumberFormatException e) {
+				output = "invalid";
 			}
 
-
-
 		}
 
-
-
-
-		in = args[0];
-		int check;
-
-		try {
-			check = Integer.parseInt(args[0]);
-
-			// 1000以下の場合
-						if(check <= 1000) {
-
-							for (int i = 0; i < in.length(); i++) {
-
-								// 3のつく場合
-								if ("3".equals(String.valueOf(in.charAt(i)))) {
-									// 3の倍数でかつ3のつく1000以下の非負整数の場合
-									if (0 == check % 3) {
-										output = "dumb";
-										break;
-										// 3のつく1000以下の非負整数の場合
-									} else {
-										output = "stupid";
-										break;
-									}
-								}
-							}
-
-							// 3のつかない場合
-							if(output ==null) {
-								// 3の倍数の1000以下の非負整数の場合
-								if (0 == check % 3) {
-									output = "idiot";
-								} else {
-									output = "smart";
-								}
-							}
-						} else {
-							output = "invalid";
-						}
-
-
-
-		} catch (NumberFormatException e) {
-			output = "invalid";
-		}
-
-
-
-
-
-
-
-			System.out.println(output);
+		System.out.println(output);
 
 	}
 }
